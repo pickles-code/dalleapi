b051d60d828b84b69d4c60f4dbb631c77846243e
pickles-code <todesign0515@gmail.com>
2023-04-08 17:48:19 +0900

    openaiapiで画像を取得

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
@@ -11,12 +13,31 @@ function App() {
 
   const generateImage = async () => {
     const res = await openai.createImage({
-      prompt: "a white siamese cat",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
+
+    setResult(res.data.data[0].url);
   };
-  return <div></div>;
+  return (
+    <div className="app-main">
+      <h1>Generate an Image using OpenAI API</h1>
+      <input
+        type="text"
+        className="app-input"
+        placeholder="Type something to Generate an Image.."
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button onClick={generateImage}>Generate an Image</button>
+
+      {result.length > 0 ? (
+        <img className="result-image" src={result} alt={prompt} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default App;
